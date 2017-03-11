f516850883a7232e6680109ca6dddb797470363e
FazioNico <contact@nicolasfazio.ch>
2017-03-11 19:35:31 +0100

    fix related element with fields items

---
+++ dev/www/template_parts/elements/fields/label-date_creation.php
@@ -0,0 +1,14 @@
+<?php
+# @Author: Nicolas Fazio <webmaster-fazio>
+# @Date:   11-03-2017
+# @Email:  contact@nicolasfazio.ch
+# @Last modified by:   webmaster-fazio
+# @Last modified time: 11-03-2017
+
+?>
+<div class="field field--name-field-date-of-creation field--type-datetime field--label-inline">
+    <div class="field__label">Date of creation</div>
+    <div class="field__item">
+      <time datetime="2016-11-09T12:00:00Z" class="datetime">2016-11-09</time>
+    </div>
+</div>
---
+++ dev/www/template_parts/elements/fields/label-location.php
@@ -0,0 +1,12 @@
+<?php
+# @Author: Nicolas Fazio <webmaster-fazio>
+# @Date:   11-03-2017
+# @Email:  contact@nicolasfazio.ch
+# @Last modified by:   webmaster-fazio
+# @Last modified time: 11-03-2017
+
+?>
+<div class="field field--name-field-location field--type-string field--label-inline">
+    <div class="field__label">Location</div>
+    <div class="field__item">Dharavi, Mumbai</div>
+</div>
